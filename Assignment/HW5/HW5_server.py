76bb79347279d2aa512149c8fdfbcecb6f0cc614
engh0205@naver.com <engh0205@naver.com>
2023-03-28 16:52:54 +0900

    Network

--- Assignment/HW5/HW5_server.py
+++ Assignment/HW5/HW5_server.py
@@ -9,21 +9,59 @@ while True:
     msg = data.decode()
     req = msg.split('\r\n')
     #파싱해서 파일 이름만 빼온 뒤
-    
-
-    if():#파일 존재 
+    a = req[0].split(" ")
+    a = a[1]
+    a = a[1:]
+    if a == "index.html":
+        f = open(a,'r',encoding='utf-8')
+        mimeType = 'text/html'
+        temp = 'Content-Type: ' + mimeType + '\r\n'
         c.send(b'HTTP/1.1 200 OK\r\n')
-        c.send(b'Content-Type: ' + mimeType + '\r\n')
+        c.send(temp.encode())
         c.send(b'\r\n')
-        ~~
+        data = f.read()
+        c.send(data.encode('euc-kr'))
+        c.close()
+    elif a == "iot.png":
+        f = open(a,'rb')
+        mimeType = 'image/png'
+        temp = 'Content-Type: ' + mimeType + '\r\n'
+        c.send(b'HTTP/1.1 200 OK\r\n')
+        c.send(temp.encode())
+        c.send(b'\r\n')
+        data = f.read()
+        c.send(data)
+        c.close()
+    elif a == "favicon.ico":
+        f = open(a,'rb')
+        mimeType = 'image/x-icon'
+        temp = 'Content-Type: ' + mimeType + '\r\n'
+        c.send(b'HTTP/1.1 200 OK\r\n')
+        c.send(temp.encode())
+        c.send(b'\r\n')
+        data = f.read()
+        c.send(data)
+        c.close()
     else:
         c.send(b'HTTP/1.1 404 Not Found\r\n')
         c.send(b'\r\n')
         c.send(b'<HTML><HEAD><TITLE>Not Found</TITLE></HEAD>')
         c.send(b'<BODY>Not Found</BODY></HTML>')
-        
+        c.close()
+
+s.close()
+    
+
+"""     if():#파일 존재 
+
+    else:
+        c.send(b'HTTP/1.1 404 Not Found\r\n')
+        c.send(b'\r\n')
+        c.send(b'<HTML><HEAD><TITLE>Not Found</TITLE></HEAD>')
+        c.send(b'<BODY>Not Found</BODY></HTML>')
+         """
 
 
 
 # 웹 서버 코드 작성
-# 각 객체(파일 또는 문자열) 전송 후, 소켓 닫기(c.close())
+# 각 객체(파일 또는 문자열) 전송 후, 소켓 닫기(c.close())``
