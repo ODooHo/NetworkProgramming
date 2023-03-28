453f9fc53e18594eb4a2f5ab74dc0f9563af240c
engh0205@naver.com <engh0205@naver.com>
2023-03-28 10:08:15 +0900

    Network

---
+++ Assignment/HW1_DOOHO_O/hw2.py
@@ -0,0 +1,13 @@
+answer = 'My name is ODooHo'
+
+
+print(len(answer))
+print(answer * 10)
+print(answer[0])
+print(answer[0:4])
+print(answer[-4:])
+print(answer[::-1])
+print(answer[1:len(answer)-1])
+print(answer.upper())
+print(answer.lower())
+print(answer.replace('a','e'))
